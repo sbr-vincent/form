c0df64d6d222d1f97281ec1a1efb00122d4c6ef7
sbr-vincent <vincent.g.sbr@gmail.com>
2021-09-01 16:31:15 -0700

    Added Form

---
+++ src/components/form.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from  'react';
+    
+    
+const UserForm = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+    const [confirm, setConfirm] = useState("");  
+
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = { firstName, lastName, email, password, confirm };
+        console.log("Welcome", newUser);
+    };
+    
+    return(
+        <>
+        <form onSubmit={ createUser }>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+            </div>
+            <div>
+                <label>Email: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="password" onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="password" onChange={ (e) => setConfirm(e.target.value) } />
+            </div>
+        </form>
+        <p>Your form data</p>
+        <p>First Name: {firstName}</p>
+        <p>Last Name: {lastName}</p>
+        <p>Email: {email}</p>
+        <p>Password: {password}</p>
+        <p>Confirm Password: {confirm}</p>
+
+        </>
+    );
+};
+    
+export default UserForm;
